c62f2901410249e5933d20c18934669b0c6e7a88
Petchie <53936910+PatrickPetch@users.noreply.github.com>
2020-08-23 23:25:42 +0800

    Initialising

---
+++ precache-manifest.a25c1243a522e370b432dfcdbaf68676.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0784214bda8850c6dbb650889e79f92",
+    "url": "/zvezda/index.html"
+  },
+  {
+    "revision": "b9169b023ab3e91d6a9d",
+    "url": "/zvezda/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b1c86cc34f4519449064",
+    "url": "/zvezda/static/js/2.4bb0c437.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/zvezda/static/js/2.4bb0c437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9169b023ab3e91d6a9d",
+    "url": "/zvezda/static/js/main.6b3084e6.chunk.js"
+  },
+  {
+    "revision": "39bd6dec29269ddad41f",
+    "url": "/zvezda/static/js/runtime-main.9980b697.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zvezda/static/media/logo.5d5d9eef.svg"
+  }
+]);
